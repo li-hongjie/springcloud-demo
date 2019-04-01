a9723ded77de6079a222295b83328792dbdb7b8c
li-hongjie <82899550@qq.com>
2019-04-01 20:52:54 +0800

    add admin,config,registry modules

--- registry/src/main/java/com/github/lihongjie/springclouddemo/registry/RegistryApplication.java
+++ registry/src/main/java/com/github/lihongjie/springclouddemo/registry/RegistryApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class RegisterApplication {
+public class RegistryApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(RegisterApplication.class, args);
+        SpringApplication.run(RegistryApplication.class, args);
     }
 
 }
